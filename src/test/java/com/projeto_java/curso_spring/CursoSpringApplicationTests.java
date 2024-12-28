7b877fc0506673846c1857b0f0becaa4f4bd2c8b
Thiago <thiagodacostaalves35@gmail.com>
2024-12-28 10:05:39 -0300

    Project-created

---
+++ src/test/java/com/projeto_java/curso_spring/CursoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto_java.curso_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
